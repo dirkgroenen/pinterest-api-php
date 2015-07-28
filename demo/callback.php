8853ca4dc9d555ce8ce73442b737d32563a72b8c
Dirk Groenen <dirk@bitlabs.nl>
2015-07-28 18:13:15 +0200

    Removed subdirectory, updated autoload and added error reoprting

--- demo/callback.php
+++ demo/callback.php
@@ -3,5 +3,8 @@
 
     $pinterest = new DirkGroenen\Pinterest\Pinterest("4781746819187084805", "391dfbaa705cfbdfb15c5cc869dffdd4fbd43d0d2b38f327b64cc668d52934ff");
 
-    $pinterest->auth->getOAuthToken()
+    if( isset($_GET["access_token"]) )
+        $pinterest->setAccessToken( $_GET["access_token"] );
+
+    
 ?>
--- demo/index.php
+++ demo/index.php
@@ -1,9 +1,12 @@
 <?php 
+    ini_set('error_reporting', E_ALL);
+    ini_set("display_errors", 1);
+
     require_once "../vendor/autoload.php";
 
     $pinterest = new DirkGroenen\Pinterest\Pinterest("4781746819187084805", "391dfbaa705cfbdfb15c5cc869dffdd4fbd43d0d2b38f327b64cc668d52934ff");
 
-    $url = $pinterest->auth->getLoginUrl( "https://" . $_SERVER["HTTP_HOST"] . "/demo/callback.php" );
+    $url = $pinterest->auth->getLoginUrl( "https://" . $_SERVER["HTTP_HOST"] . "/Pinterest-API-PHP/demo/callback.php" );
 
     echo "<a href='" . $url . "'>Authorize</a>";
 ?>
