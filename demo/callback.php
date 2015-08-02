e4e8d980e6de32fa47eacf7baad386fc1e1938d7
Dirk Groenen <dirk@bitlabs.nl>
2015-08-02 23:10:56 +0200

    Removed keys (and app so the previous won't work

--- demo/callback.php
+++ demo/callback.php
@@ -17,24 +17,5 @@
      * 
      * @var DirkGroenen\Pinterest\Pinterest
      */
-    $pinterest = new DirkGroenen\Pinterest\Pinterest("4781746819187084805", "391dfbaa705cfbdfb15c5cc869dffdd4fbd43d0d2b38f327b64cc668d52934ff");
-
-    /**
-     * Get the access_token from the query string or cookie
-     */
-    if( isset($_GET["access_token"]) ){
-        setcookie("access_token", $_GET["access_token"], time()+3600*24*30);
-    }
-    else if(!isset($_COOKIE["access_token"])){
-        echo "No access token received";
-        die();
-    }
-    
-    // Set the access token
-    $pinterest->auth->setOAuthToken( $_COOKIE["access_token"]  );
-
-    // Get ben and get his pins
-    $boards = $pinterest->users->getMeLikes();
-
-    echo $boards;
-?>
+    $pinterest = new DirkGroenen\Pinterest\Pinterest("", "");
+?> 
