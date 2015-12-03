0ad77477b6f1ede358b3334b54e3283bfac8e778
Nicolas Widart <n.widart@gmail.com>
2015-12-03 10:03:04 +0100

    Adding @properties on Pinterest class to enable auto completion in IDEs

--- src/Pinterest/Pinterest.php
+++ src/Pinterest/Pinterest.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 /**
- * Copyright 2015 Dirk Groenen 
+ * Copyright 2015 Dirk Groenen
  *
  * (c) Dirk Groenen <dirk@bitlabs.nl>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -15,33 +15,39 @@ use DirkGroenen\Pinterest\Utils\CurlBuilder;
 use DirkGroenen\Pinterest\Transport\Request;
 use DirkGroenen\Pinterest\Exceptions\InvalidEndpointException;
 
+/**
+ * @property \DirkGroenen\Pinterest\Endpoints\Boards boards
+ * @property \DirkGroenen\Pinterest\Endpoints\Following following
+ * @property \DirkGroenen\Pinterest\Endpoints\Pins pins
+ * @property \DirkGroenen\Pinterest\Endpoints\Users users
+ */
 class Pinterest {
-    
+
     /**
      * Reference to authentication class instance
-     * 
+     *
      * @var Auth\PinterestOAuth
      */
     public $auth;
 
     /**
      * A reference to the request class which travels
-     * through the application 
-     * 
+     * through the application
+     *
      * @var Transport\Request
      */
     public $request;
 
     /**
      * A array containing the cached endpoints
-     * 
+     *
      * @var array
      */
     private $cachedEndpoints = [];
 
     /**
      * Constructor
-     * 
+     *
      * @param  string       $client_id
      * @param  string       $client_secret
      * @param  CurlBuilder  $curlbuilder
@@ -64,24 +70,24 @@ class Pinterest {
      *
      * @access public
      * @param string    $endpoint
-     * @return mixed 
+     * @return mixed
      * @throws Exceptions\InvalidEndpointException
      */
     public function __get($endpoint)
     {
         $endpoint = strtolower($endpoint);
         $class = "\\DirkGroenen\\Pinterest\\Endpoints\\" . ucfirst($endpoint);
-        
+
         // Check if an instance has already been initiated
         if(!isset($this->cachedEndpoints[$endpoint])){
             // Check endpoint existence
             if(!class_exists($class))
                 throw new InvalidEndpointException;
 
-            // Create a reflection of the called class and initialize it 
+            // Create a reflection of the called class and initialize it
             // with a reference to the request class
             $ref = new \ReflectionClass($class);
-            $obj = $ref->newInstanceArgs([ $this->request, $this ]);        
+            $obj = $ref->newInstanceArgs([ $this->request, $this ]);
 
             $this->cachedEndpoints[$endpoint] = $obj;
         }
