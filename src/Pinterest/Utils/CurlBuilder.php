f0e2e72e3667a0975f13b50ec32bc87a50bdce16
Ziga Berce <bercium@gmail.com>
2016-01-06 08:23:30 +0100

    Trim header value

--- src/Pinterest/Utils/CurlBuilder.php
+++ src/Pinterest/Utils/CurlBuilder.php
@@ -161,7 +161,7 @@ class CurlBuilder {
         foreach(explode("\n",$headers) as $row)
         {
             $header = explode(':', $row,2);
-            if (count($header) == 2) $result[$header[0]] = $header[1];
+            if (count($header) == 2) $result[$header[0]] = trim($header[1]);
             else $result[] = $header[0];
         }
         return $result;
