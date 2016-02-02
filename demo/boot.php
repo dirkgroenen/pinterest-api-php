0c8efdb7ed19ce2676eccbfc3ceecc45a423e167
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-02-02 16:32:22 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- demo/boot.php
+++ demo/boot.php
@@ -15,11 +15,9 @@
         $pinterest->auth->setOAuthToken($token->access_token);
 
         setcookie("access_token", $token->access_token);
-    }
-    else if (isset($_GET["access_token"])) {
+    } else if (isset($_GET["access_token"])) {
         $pinterest->auth->setOAuthToken($_GET["access_token"]);
-    }
-    else if (isset($_COOKIE["access_token"])) {
+    } else if (isset($_COOKIE["access_token"])) {
         $pinterest->auth->setOAuthToken($_COOKIE["access_token"]);
     }
 
