74b0c2a250a1651d78997559fcab4742bd8c2e66
Dirk Groenen <dirk@bitlabs.nl>
2016-02-02 17:28:27 +0100

    Create board on callback

--- demo/callback.php
+++ demo/callback.php
@@ -7,7 +7,9 @@
         <h1>Hey you!</h1>
         <h2>$pinterest->boards->create()</h2>
 
-        <?php echo $pinterest->users->getMeBoards(); ?>
+        <?php echo $pinterest->boards->create(array(
+            "name" => "Test from Pinterest API"
+        )); ?>
 
     </body>
 </html>
