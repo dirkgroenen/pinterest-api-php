db14bab6035769b44592bca1aef31826fea5c7a8
Dirk Groenen <dirk@bitlabs.nl>
2016-02-05 16:29:04 +0100

    Change in demo

--- demo/callback.php
+++ demo/callback.php
@@ -7,8 +7,7 @@
         <h1>Hey you!</h1>
         <h2>$pinterest->pins->create()</h2>
 
-        <?php echo $pinterest->pins->create(array(
-            "board"         => "pinterest-api-test",
+        <?php echo $pinterest->pins->delete("503066220857361559", array(
             "note"          => "Test from API",
             "image_url"     => "https://download.unsplash.com/photo-1438216983993-cdcd7dea84ce"
         )); ?>
