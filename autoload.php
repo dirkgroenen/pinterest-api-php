5a249842428a5c7d1f238cb228b23a8680e830ee
Dirk Groenen <dirk@bitlabs.nl>
2016-02-10 21:10:41 +0100

    Fix src

--- autoload.php
+++ autoload.php
@@ -15,7 +15,7 @@ spl_autoload_register(function ($class) {
     $prefix = 'DirkGroenen\\Pinterest\\';
 
     // base directory for the namespace prefix
-    $base_dir = __DIR__ . '/src/';
+    $base_dir = __DIR__ . '/src/Pinterest/';
 
     // does the class use the namespace prefix?
     $len = strlen($prefix);
