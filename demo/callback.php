e3a0cfdaaaee4621a3d2d7da24b300c08c626804
Dirk Groenen <dirk@bitlabs.nl>
2016-02-11 10:40:16 +0100

    add http_build_query to POST, fixes #39

--- demo/callback.php
+++ demo/callback.php
@@ -10,9 +10,12 @@
                 "description"   => "Test"
             ));*/
 
-            echo $pinterest->boards->edit("dirkgroenen/test-from-api", array(
-                "name"          => "Noted update 2 - API"
-            ), "id,name,url,description,creator,created_at,counts,image");
+            echo $pinterest->pins->create(array(
+                "board"         => "dirkgroenen/test-from-api",
+                'image_url'     => 'https://images.unsplash.com/photo-1453974336165-b5c58464f1ed?crop=entropy&fit=crop&fm=jpg&h=1000&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1925',
+                'note'          => 'test',
+                'link'          => 'http://tld.com/072AE601DF7DB00445386F5C9CC46F74'
+            ));
         ?>
 
     </body>
